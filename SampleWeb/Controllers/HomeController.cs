05674b835e3c4966967affc1d9fb20fcffedad5d
Jishnu A <jishnu.aranhampurath@neudesic.com>
2021-09-01 11:39:44 +0530

    first commit

---
+++ SampleWeb/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SampleWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration config)
+        {
+            configuration = config;
+        }
+        public IActionResult Index()
+        {
+            string conString = ConfigurationExtensions.GetConnectionString(configuration, "DefaultConnection");
+            SqlConnection con = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand("SELECT 1", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
